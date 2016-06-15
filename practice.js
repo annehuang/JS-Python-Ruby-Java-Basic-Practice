67aafca10814126130a91894d3982887da7e187d
annehuang <nehuang@gmail.com>
2016-06-15 15:12:41 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,7 +1,26 @@
+// Practicing with Objects
+
 var trips = [{name:"Chicago", season:"summer", activity1:"music festival", activity2:"bean"},
-              {name:"Hong Kong", season:"fall", activity1:"Ocean Park", activity2:"Disney"},
-              {name:"Philadelphia", season:"spring", activity1:"go to school", activity2:"ride the train"}
+              {name:"Hong Kong", season:"spring", activity1:"Ocean Park", activity2:"Disney"},
+              {name:"Philadelphia", season:"fall", activity1:"go to school", activity2:"ride the train"}
 ];
+
+// alternatively
+var trip1 = new Object();
+trip1.name = "Chicago";
+trip1.season = "summer";
+trip1.activity1 = "music festival";
+trip1.activity2= "bean";
+
+// alternatively
+function trip(name, season, activity1, activity2) {
+  this.name = name;
+  this.season = season;
+  this.activity1 = activity1;
+  this.activity2 = activity2;
+}
+
+var tripNo1 = new trip("Chicago", "summer", "music festival", "bean");
 
 // retuns the count of number of words in string str1
 function countWords(str1){
