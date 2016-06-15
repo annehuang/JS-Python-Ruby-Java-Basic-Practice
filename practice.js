ab308026cbd834cdfc46e6dbe3753e72b8d1f2fd
annehuang <nehuang@gmail.com>
2016-06-15 14:52:29 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,10 @@
 var trips = [{name:"Chicago"}]
+
+
+function countWords(str1){
+  var count = 0;
+  for (var i = 1; i < str1.length; i++){
+      if (str1[i] === " ")
+        count++;
+  }      
+}
