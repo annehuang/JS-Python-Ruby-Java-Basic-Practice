a9243644a348261d8736a00e3cccdd42444e4319
Anne Huang <nehuang@gmail.com>
2016-06-24 23:10:32 -0500

    Update and rename FractionArray.py to FractionLst.py

---
+++ FractionLst.py
@@ -0,0 +1,23 @@
+# Print the fraction of numbers in the list that are positive, negative, or zero.
+
+arr = [10, -5, 6, 20, 11, 100]
+
+n = len(arr)
+
+sumPos = 0
+sumNeg = 0
+sumZ = 0
+
+for i in range(0, n):
+  if arr[i] > 0:
+    sumPos += 1
+  elif arr[i] < 0:
+    sumNeg += 1
+  else:
+    sumZ += 1
+
+print(sumPos/n)
+
+print(sumNeg/n)
+
+print(sumZ/n)
