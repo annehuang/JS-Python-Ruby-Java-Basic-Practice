3cf935ed5f8634b225ecdb389eb2a2cb187f1b26
Anne Huang <nehuang@gmail.com>
2016-07-02 16:42:45 -0500

    Update FractionLst.py

--- FractionLst.py
+++ FractionLst.py
@@ -2,22 +2,25 @@
 
 arr = [10, -5, 6, 20, 11, 100]
 
-n = len(arr)
+def fractionList(arr):
+  n = len(arr)
 
-sumPos = 0
-sumNeg = 0
-sumZ = 0
+  sumPos = 0
+  sumNeg = 0
+  sumZ = 0
 
-for i in range(0, n):
-  if arr[i] > 0:
-    sumPos += 1
-  elif arr[i] < 0:
-    sumNeg += 1
-  else:
-    sumZ += 1
+  for i in range(0, n):
+    if arr[i] > 0:
+      sumPos += 1
+    elif arr[i] < 0:
+      sumNeg += 1
+    else:
+      sumZ += 1
 
-print(sumPos/n)
+  print(sumPos/n)
 
-print(sumNeg/n)
+  print(sumNeg/n)
 
-print(sumZ/n)
+  print(sumZ/n)
+
+fractionList(arr)
