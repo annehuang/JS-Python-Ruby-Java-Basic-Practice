cf5c83f4fed4763b50fc6f607fc2096aeb01ddcd
Anne Huang <nehuang@gmail.com>
2016-07-02 16:34:25 -0500

    Update palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,15 +1,16 @@
-largest = 0 
+largest = 0 # Identify largest palindrome that is the product of 2 3-digit numbers
+
 for i in 100...999 do
 	for j in 100...999 do
 		product = i * j
 
 		if product.to_s == product.to_s.reverse
 			palindrome = product
-			if palindrome > largest
+			if palindrome > largest # If this is the largest palindrome found so far
 				largest = palindrome
 			end
 		end
 	end
 end
 
-puts largest
+puts largest # print largest palindrome
