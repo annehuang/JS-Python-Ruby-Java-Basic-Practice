e4a55500bdfef576de2ca68f4f41c3ef0e7dbc8e
Anne Huang <nehuang@gmail.com>
2016-07-02 16:39:31 -0500

    Update diagonalDifference.py

--- diagonalDifference.py
+++ diagonalDifference.py
@@ -1,13 +1,20 @@
 a = [[9, 44, 9], [99, 3, 9], [10, 3, 2]]
 
-n = len(a)
+# pre: a is a 3*3 2-D array of integers
+def diagonalDifference(a):
+    n = len(a)
 
-sumA = 0
-for i in range(0, n):
-    sumA += a[i][i]
-sumB = 0
+    sumA = 0
+    for i in range(0, n):
+        sumA += a[i][i]
+    # find the sum of all the entries in the diagonal that begins in the upper left corner
 
-for i in range(0, n):
-    sumB += a[i][n-1-i]
+    sumB = 0
+    for i in range(0, n):
+        sumB += a[i][n-1-i]
+    # find the sume of all the entries in the diagonal that begins in the upper right corner
 
-print(abs(sumA - sumB))
+    print(abs(sumA - sumB))
+    # print the absolute difference between the 2 diagonals
+
+diagonalDifference(a)
