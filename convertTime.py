a5f59a7f8785b39f7f6ae8ee6a744b23bc0db7c0
Anne Huang <nehuang@gmail.com>
2016-07-02 16:41:31 -0500

    Update convertTime.py

--- convertTime.py
+++ convertTime.py
@@ -1,13 +1,15 @@
-time = "7:52:52PM"
+time = "07:52:52PM"
 
-
-hour = int(time[0:2])
-min = time[3:5]
-sec = time[6:8]
-if hour != 12 and time[8:10] == “PM”:
-	hour += 12
-if hour == 12 and time[8:10] == “AM”:
-	hour = 0
-if hour < 10:
-	hour = “0” + str(hour)
-print(str(hour) + “:” + min + “:” + sec)
+#pre: time is a string in "HH:MM:SSAM" format
+#post: convert to military time
+def convertTime(time):
+	hour = int(time[0:2])
+	min = time[3:5]
+	sec = time[6:8]
+	if hour != 12 and time[8:10] == “PM”:
+		hour += 12
+	if hour == 12 and time[8:10] == “AM”:
+		hour = 0
+	if hour < 10:
+		hour = “0” + str(hour)
+	print(str(hour) + “:” + min + “:” + sec)
