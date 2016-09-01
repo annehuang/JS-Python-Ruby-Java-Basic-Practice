f20035f8efb05b21f02245f51054d7ef5831d76e
Anne Huang <nehuang@gmail.com>
2016-09-01 15:43:43 -0500

    Create readFile.c

---
+++ readFile.c
@@ -0,0 +1,18 @@
+// a simple program to read a file into memory and print it out to stdout
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv){
+  char buf[4096];
+
+  // read whole file into memory
+  int read_fd = open("filename", O_RDONLY, 0);
+
+  read(read_fd, buf, 4096);
+
+  printf("%s\n", buf);
+
+return 0;
+}
