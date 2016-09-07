f9c5b7900d06682cc38003925488c661e3a82700
Anne Huang <nehuang@gmail.com>
2016-09-06 21:24:47 -0500

    Update copyArray.c

--- copyArray.c
+++ copyArray.c
@@ -10,9 +10,19 @@ void foo(char* buf)
         strncpy(buf, pbuf, 5);
 }
 
+void unsignedFoo(unsigned char * buf){
+        unsigned char * pbuf = "1234";
+        printf("%s\n", (char *) pbuf);
+        memcpy(buf, pbuf, 4);
+}
+
 int main(int argc, char** argv){
         char buf[5];
         foo(buf);
         printf("%s\n", buf);
+        
+        char buf2[4];
+        unsignedfoo(buf2);
+        printf("%s\n", buf2);
 }
 ~
