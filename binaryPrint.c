d3d276ef7d31ae245404302f4ba066ba05d8ba95
Anne Huang <nehuang@gmail.com>
2016-12-17 21:20:34 -0600

    Update binaryPrint.c

--- binaryPrint.c
+++ binaryPrint.c
@@ -1,6 +1,6 @@
 static void binaryprint_le(uint8_t * binaryBuf, const uint8_t * data, size_t size)
 {
-
+// write in Little Endian order
  	while (size--){
    		*binaryBuf = *(data + size);
 		printf("%02hhX", *binaryBuf);
