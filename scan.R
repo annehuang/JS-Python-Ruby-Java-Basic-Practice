400ce61d65f949c5531c8166d1da606890b99103
Anne Huang <nehuang@gmail.com>
2017-05-16 21:54:32 -0500

    Update scan.R

--- scan.R
+++ scan.R
@@ -1,1 +1,3 @@
-> x <- scan("text.txt", what=integer(), sep="\n")
+#scan reads in input.
+#The what parameter tells you what type to read input in as.
+x <- scan("text.txt", what=integer(), sep="\n")
